c00e3557bbe224c663cedb866fc141d375b2c73f
sfranky <sfranky@gmail.com>
2015-09-29 01:58:31 +0300

    changed process_line to return nested key-value constructs

--- tests/test_yaml_parser.py
+++ tests/test_yaml_parser.py
@@ -5,32 +5,32 @@ from yaml_parser import *
 
 
 @pytest.mark.parametrize('fin, t',
+    (
         (
-            (
-                ['scheduler: sge'],
-                [0, "scheduler:", "sge"]
-            ),
-            (
-                ['schedulers:'],
-                [0, "schedulers:"]
-            ),
-            (
-                ['  pbs:'],
-                [1, "pbs:"]
-            ),
-            (
-                ["   - r'moonshot'"],
-                [1, "-", "r'moonshot'"]
-            ),
-            (
-                [" - '\w*cms048': Blue"],
-                [1, "-", "'\\w*cms048': Blue"]
-            ),
-            (
-                ["term_size: [53, 176]"],
-                [0, "term_size:", "[53, 176]"]
-            ),
+            ['scheduler: sge'],
+            [0, "scheduler:", "sge"]
         ),
+        (
+            ['schedulers:'],
+            [0, "schedulers:"]
+        ),
+        (
+            ['  pbs:'],
+            [1, "pbs:"]
+        ),
+        (
+            ["   - r'moonshot'"],
+            [1, "-", "r'moonshot'"]
+        ),
+        (
+            [" - '\w*cms048': Blue"],
+            [1, "-", "'\\w*cms048': Blue"]
+        ),
+        (
+            ["term_size: [53, 176]"],
+            [0, "term_size:", "[53, 176]"]
+        ),
+    ),
 )
 def test_get_line(fin, t):
     get_lines = get_line(fin, verbatim=False)
@@ -39,43 +39,117 @@ def test_get_line(fin, t):
 
 
 @pytest.mark.parametrize('fin, t',
+    (
         (
-            (
-                [
-                    '  oar:\n',
-                    '    oarnodes_s_file: oarnodes_s_Y.txt\n'
-                ],
-                [
-                    [1, "oar:"],
-                    [1, 'oarnodes_s_file:', 'oarnodes_s_Y.txt']
-                ]
-            ),
-            (
-                [
-                    '    oarstat_file: oarstat.txt\n',
-                    '  sge:'
-                ],
-                [
-                    [1, "oarstat_file:", "oarstat.txt"],
-                    [-1, "sge:"]
-                ]
-            ),
-            (
-                [
-                    '    sge_file_stat: qstat.F.xml.stdout',
-                    '\n',
-                    'faster_xml_parsing: False'
-                ],
-                [
-                    [1, 'sge_file_stat:', 'qstat.F.xml.stdout'],
-                    [-1],
-                    [0, 'faster_xml_parsing:', 'False'],
-                ]
-            )
+            [
+                '  oar:\n',
+                '    oarnodes_s_file: oarnodes_s_Y.txt\n'
+            ],
+            [
+                [1, "oar:"],
+                [1, 'oarnodes_s_file:', 'oarnodes_s_Y.txt']
+            ]
         ),
+        (
+            [
+                '    oarstat_file: oarstat.txt\n',
+                '  sge:'
+            ],
+            [
+                [1, "oarstat_file:", "oarstat.txt"],
+                [-1, "sge:"]
+            ]
+        ),
+        (
+            [
+                '    sge_file_stat: qstat.F.xml.stdout',
+                '\n',
+                'faster_xml_parsing: False'
+            ],
+            [
+                [1, 'sge_file_stat:', 'qstat.F.xml.stdout'],
+                [-1],
+                [0, 'faster_xml_parsing:', 'False'],
+            ]
+        )
+    ),
 )
 def test_get_more_lines(fin, t):
-    get_lines = get_line(fin, verbatim=False)
+    get_lines = get_line(fin)
     for idx, line in enumerate(fin):
         actual_t = next(get_lines)
         assert actual_t == t[idx]
+
+
+# @pytest.mark.parametrize('line, fin, get_lines, parent_container, container_stack, stack',
+# @pytest.mark.parametrize('fin, line, get_lines, key_container',  # get_line(fin)
+#      (
+#          (
+#              ['testkey: testvalue'], [0, 'testkey:', 'testvalue'], get_line(['testkey: testvalue']), {'testkey:': 'testvalue'}
+#          ),
+#          (
+#              ['testkey:'], [0, 'testkey:'], get_line(['testkey:']), {'testkey:': {}}
+#          ),
+#          (
+#              ['testkey: [testvalue]'], [0, 'testkey:', '[testvalue]'], get_line(['testkey: [testvalue]']), {'testkey:': '[testvalue]'}
+#          ),
+#          (
+#              ['testkey: |'], [0, 'testkey:', '|'], get_line(['testkey: |']), {'testkey:': '|'}
+#          ),
+#          (
+#              ['- testkey:'], [0, '-', 'testkey:'], get_line(['- testkey:']), {'-': 'testkey:'}
+#          ),
+#          (
+#              ['- testkey: testvalue'], [0, '-', 'testkey: testvalue'], get_line(['- testkey: testvalue']), {'-': 'testkey: testvalue'}
+#          ),
+#          (
+#              ['- testkey: [testvalue]'], [0, '-', 'testkey: [testvalue]'], get_line(['- testkey: [testvalue]']), {'-': 'testkey: [testvalue]'}
+#          ),
+#          (
+#              ['- testvalue'], [0, '-', 'testvalue'], get_line(['- testvalue']), {'-': 'testvalue'}
+#          ),
+#      )
+# )
+# def test_process_line(line, fin, get_lines, key_container):  # parent_container, container_stack, stack):
+#     assert key_container == process_line(line, fin, get_lines)
+
+@pytest.mark.parametrize('fin, line, get_lines, key_container',  # get_line(fin)
+     (
+         (
+             ['testkey: testvalue'], [0, 'testkey:', 'testvalue'], get_line(['testkey: testvalue']), {'testkey:': 'testvalue'}
+         ),
+         (
+             ['testkey:'], [0, 'testkey:'], get_line(['testkey:']), {'testkey:': {}}
+         ),
+         (
+             ['testkey: [testvalue]'], [0, 'testkey:', '[testvalue]'], get_line(['testkey: [testvalue]']), {'testkey:': '[testvalue]'}
+         ),
+         (
+             ['testkey: |'], [0, 'testkey:', '|'], get_line(['testkey: |']), {'testkey:': '|'}
+         ),
+         (
+             ['- testkey:'], [0, '-', 'testkey:'], get_line(['- testkey:']), {'-': {'testkey:': {}}}
+         ),
+         (
+             ['- testkey: testvalue'], [0, '-', 'testkey: testvalue'], get_line(['- testkey: testvalue']), {'-': {'testkey:': 'testvalue'}}
+         ),
+         (
+             ['- testkey: [testvalue]'], [0, '-', 'testkey: [testvalue]'], get_line(['- testkey: [testvalue]']), {'-': {'testkey:': '[testvalue]'}}
+         ),
+         (
+             ['- testvalue'], [0, '-', 'testvalue'], get_line(['- testvalue']), {'-': 'testvalue'}
+         ),
+     )
+)
+def test_process_line(line, fin, get_lines, key_container):  # parent_container, container_stack, stack):
+    assert key_container == process_line(line, fin, get_lines)
+
+# @pytest.mark.parametrize('container, result',
+#      (
+#          (
+#                  {},
+#          )
+#      )
+# )
+# def test_process_container(container):
+#     assert process_container(container) == result
--- yaml_parser.py
+++ yaml_parser.py
@@ -5,7 +5,7 @@ from itertools import takewhile, dropwhile
 conf_file = '/home/sfranky/.local/qtop/qtopconf.yaml'
 
 
-def get_line(fin, verbatim=False):
+def get_line(fin, verbatim=False, SEPARATOR=None):
     """
     Yields a list per line read, of the following form:
     [indentation_change, line up to first space, line after first space if exists]
@@ -16,6 +16,7 @@ def get_line(fin, verbatim=False):
     0: same indentation
     -1: line is unindent
     Empty lines only return the indentation change.
+    Where the line splits depends on SEPARATOR (default is first space)
     """
     indent = 0
     for line in fin:
@@ -33,79 +34,47 @@ def get_line(fin, verbatim=False):
         else:
             d_indent = -1
         line = line.rstrip()
-        yield verbatim and [d_indent, line] or [d_indent] + line.split(None, 1)
+        yield verbatim and [d_indent, line] or [d_indent] + line.split(None or SEPARATOR, 1)
 
 
-# def read_yaml_config(fn):
-#     raw_key_values = {}
-#     with open(fn, mode='r') as fin:
-#         get_lines = get_line(fin)
-#         line = next(get_lines)
-#         # raw_key_values[line[1]] = line[2]
-#         # while line[0] >= 0 and len(line) > 1:]
-#         while line:
-#             block = dict()
-#             block, line = _read_yaml_config_block(line, fin, get_lines, block)
-#             # for key in block:
-#             #     print key
-#             #     print '\t' + str(block[key])
-#             raw_key_values.update(block)
-#
-#         config_dict = dict([(key, value) for key, value in raw_key_values.items()])
-#         return config_dict
-#
-#
-# def _read_yaml_config_block(line, fin, get_lines, block):
-#     container_stack = list()
-#     stack = 0
-#     if len(line) > 1:  # non-empty line
-#         key, container = process_key_value_line(line, fin, get_lines)
-#         block[key] = container
-#     parent_container = block
-#     while len(line) == 1:
-#         line = next(get_lines)
-#     while len(line) > 1:  # as long as a blank line is not reached (i.e. block is not complete)
-#         if line[0] > 0:  # nesting
-#             parent_container = container
-#             # container_stack.append(parent_container)
-#             stack += 1
-#             container = process_line(line, fin, get_lines, parent_container, container_stack, stack)
-#             # container_stack.append(parent_container)
-#         elif line[0] == 0:  # same level
-#             container = process_line(line, fin, get_lines, parent_container, container_stack, stack)
-#             # if not container_stack and not stack:
-#             #     container_stack.append(parent_container)
-#         elif line[0] < 0:  # go up one level
-#             container_stack.pop()
-#             stack -= 1
-#             parent_container = container_stack[-1]
-#             key, container = process_key_value_line(line, fin, get_lines)
-#             parent_container[key] = container
-#         line = next(get_lines)
-#
-#     return block, line
-#
-#
-# def process_key_value_line(line, fin, get_lines=None):
-#     key = line[1].rstrip(':')
-#     if len(line) == 3:  # key-value in same line
-#         value = line[2]
-#         if value == '|':
-#             value = process_code(fin)
-#     else:
-#         value = {}
-#         # value = None
-#     value = process_value(value, fin)
-#     return key, value
-#
-#
-# def process_list_item_line(line, fin, stack, parent_container):
-#     stack +=1
-#     value = line[-1]
-#     container = process_value(value, fin)
-#     return container, stack
-#
-#
+def read_yaml_config(fn):
+    raw_key_values = {}
+    with open(fn, mode='r') as fin:
+        get_lines = get_line(fin)
+        line = next(get_lines)
+        while line:
+            block = dict()
+            block, line = _read_yaml_config_block(line, fin, get_lines, block)
+            # for key in block:
+            #     print key
+            #     print '\t' + str(block[key])
+            raw_key_values.update(block)
+
+        config_dict = dict([(key, value) for key, value in raw_key_values.items()])
+        return config_dict
+
+
+def _read_yaml_config_block(line, fin, get_lines, block):
+    if len(line) > 1:  # non-empty line
+        key, container = process_key_value_line(line, fin, get_lines)
+        block[key] = container
+
+    while len(line) == 1:
+        line = next(get_lines)
+    while len(line) > 1:  # as long as a blank line is not reached (i.e. block is not complete)
+        if line[0] > 0:  # nesting
+            container = process_line(line, fin, get_lines, parent_container, container_stack, stack)
+        elif line[0] == 0:  # same level
+            container = process_line(line, fin, get_lines, parent_container, container_stack, stack)
+        elif line[0] < 0:  # go up one level
+            parent_container = container_stack[-1]
+            key, container = process_key_value_line(line, fin, get_lines)
+            parent_container[key] = container
+        line = next(get_lines)
+
+    return block, line
+
+
 # def process_line(line, fin, get_lines, parent_container, container_stack, stack):
 #     container_stack.append(parent_container)
 #     if line[1].endswith(':'):
@@ -117,7 +86,56 @@ def get_line(fin, verbatim=False):
 #         # (container is {list_out_by_name_pattern: dict()})
 #         parent_container.setdefault('-',[]).append(container)
 #     return container  # want to return a {} here for by_name_pattern
-#
+
+
+def process_line(line, fin, get_lines):
+    key = line[1]
+    if len(line) == 2:
+        container = {}
+    elif len(line) == 3:
+        container = line[2]
+        if ': ' in container:
+            parent_key = key
+            # parent_container = container
+            key, container = container.split(None, 1)
+            return {parent_key: {key: container}}
+        elif container.endswith(':'):
+            parent_key = key
+            key = container
+            container = {}
+            return {parent_key: {key: container}}
+    else:
+        raise ValueError("Didn't anticipate that!")
+    return {key: container}
+
+
+def process_container(line, container, fin):
+    if len(line) > 2:
+        get_nested_container = get_line([container])  # SEPARATOR=': '
+        nested_container = next(get_nested_container)[1:]
+        key, container = process_line(line, fin, get_nested_container)
+
+
+def process_key_value_line(line, fin, get_lines=None):
+    key = line[1].rstrip(':')
+    if len(line) == 3:  # key-value in same line
+        value = line[2]
+        if value == '|':
+            value = process_code(fin)
+    else:
+        value = {}
+        # value = None
+    value = process_value(value, fin)
+    return key, value
+
+
+def process_list_item_line(line, fin, stack, parent_container):
+    stack +=1
+    value = line[-1]
+    container = process_value(value, fin)
+    return container, stack
+
+
 def process_code(fin):
     # line = next(get_lines)
     # code = line[1]
@@ -132,38 +150,39 @@ def process_code(fin):
         code.append(' ' + line[-1])
         line = next(get_code)
     return ' '.join(code.strip())
-#
-# def process_value(_value, fin):
-#     if _value == 'False':
-#         return eval(_value)
-#     elif _value in [{}, None]:
-#         return _value
-#     elif isinstance(_value, list):
-#         value = _value[0].rstrip()
-#         if value.endswith(':'):
-#             key = value.rstrip(':')
-#             value = {}
-#         elif ':' in value:
-#             key, value = value.split(None, 1)
-#         else:
-#             key = '-'
-#         new_line = [0, key, value]
-#         key, value = process_key_value_line(new_line, fin, None)
-#         d = {key: value}
-#         return [d]
-#     elif ': ' in _value:
-#         key, _value = _value.split(': ')
-#         value = eval(repr(_value))
-#         d = {key: value}
-#     elif _value.endswith(':'):
-#         key = _value.rstrip(':')
-#         value = dict()
-#         d = {key: value}
-#     else:  # when it's just a value
-#         return _value
-#     return d
-#
-#
+
+
+def process_value(_value, fin):
+    if _value == 'False':
+        return eval(_value)
+    elif _value in [{}, None]:
+        return _value
+    elif isinstance(_value, list):
+        value = _value[0].rstrip()
+        if value.endswith(':'):
+            key = value.rstrip(':')
+            value = {}
+        elif ':' in value:
+            key, value = value.split(None, 1)
+        else:
+            key = '-'
+        new_line = [0, key, value]
+        key, value = process_key_value_line(new_line, fin, None)
+        d = {key: value}
+        return [d]
+    elif ': ' in _value:
+        key, _value = _value.split(': ')
+        value = eval(repr(_value))
+        d = {key: value}
+    elif _value.endswith(':'):
+        key = _value.rstrip(':')
+        value = dict()
+        d = {key: value}
+    else:  # when it's just a value
+        return _value
+    return d
+
+
 # #### MAIN ###############
 #
 # # with open(conf_file, mode='r') as fin:
