2f6088db3540a3612d5241b37037df0ff2107ff8
Sotiris Fragkiskos <sfranky@gmail.com>
2016-01-21 16:02:23 +0200

    fixed version number

--- qtop.py
+++ qtop.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 ################################################
-#              qtop v.0.8.5                    #
+#              qtop v.0.8.6                    #
 #     Licensed under MIT-GPL licenses          #
 #                     Sotiris Fragkiskos       #
 #                     Fotis Georgatos          #
