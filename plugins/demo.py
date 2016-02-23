8a3432ae0ce5c983f1d0b0fb7dc0b8de2488ac5b
Sotiris Fragkiskos <sfranky@gmail.com>
2016-02-23 19:24:24 +0200

    more colours, down nodes don't take any jobs

--- plugins/demo.py
+++ plugins/demo.py
@@ -1,6 +1,6 @@
 from serialiser import *
 import random
-WORKER_NODES = 25
+WORKER_NODES = 50
 QUEUES = 3
 
 
@@ -23,6 +23,7 @@ class DemoBatchSystem(GenericBatchSystem):
         self.scheduler_output_filenames = scheduler_output_filenames
         self.config = config
         self.jobs = list()  # needed just for the demo to work
+        self.total_nps = 0
 
     def get_worker_nodes(self):
         """
@@ -42,68 +43,18 @@ class DemoBatchSystem(GenericBatchSystem):
                 random.choice(['foo.com', 'bar.com', 'baz.com'])])
             worker_node["state"] = random.choice(["-", "b", "d"])
             worker_node["gpus"] = random.choice([0, 2, 4, 8, 16, 24, 32])
-            worker_node["np"] = random.choice([4, 8, 16, 24, 32])
+            worker_node["np"] = random.choice([8, 16, 24, 32])
+            self.total_nps += worker_node["np"]
+
             # pick a series of random core/random job nr pairs
-            # worker_node["core_job_map"] = dict((random.choice(range(worker_node["np"])), "j" + str(random.randint(0, 500)))
-            #                                    for i in range(random.randint(0, worker_node["np"])))
             worker_node["core_job_map"] = dict()
             for i in range(random.randint(0, worker_node["np"])):
+                if worker_node["state"] == "d": break
                 random_job_id = "j" + str(random.randint(0, 500))
                 random_core = random.choice(range(worker_node["np"]))
                 worker_node["core_job_map"][random_core] = random_job_id
                 self.jobs.append(random_job_id)
             worker_nodes.append(worker_node)
-        # worker_nodes = [
-        #     {
-        #         "domainname": "dn1.foo.com",
-        #         "state": "-",
-        #         "gpus": "",  # Values like this could be displayed by including them in the QTOPCONF file
-        #         "np": 24,
-        #         "core_job_map": {11: "j2", 12: "j2"}
-        #     },
-        #     {
-        #         "domainname": "dn2.bar.com",
-        #         "state": "-",
-        #         "gpus": "",
-        #         "np": 8,
-        #         "core_job_map": {0: "j2", 1: "j2"}
-        #     },
-        #     {
-        #         "domainname": "dn3.baz.com",
-        #         "state": "-",
-        #         "gpus": "",
-        #         "np": 16,
-        #         "core_job_map": {2: "j2", 4: "j1"}
-        #     },
-        #     {
-        #         "domainname": "dn4.baz.com",
-        #         "state": "b",
-        #         "gpus": "",
-        #         "np": 4,
-        #         "core_job_map": {1: "j1", 3: "j5"}
-        #     },
-        #     {
-        #         "domainname": "dn5.baz.com",
-        #         "state": "-",
-        #         "gpus": "",
-        #         "np": 16,
-        #         "core_job_map": {2: "j2", 4: "j2"}
-        #     },
-        #     {
-        #         "domainname": "sf1.baz.com",
-        #         "state": "-",
-        #         "gpus": "",
-        #         "np": 16,
-        #         "core_job_map": {2: "j2", 4: "j4"}
-        #     },
-        #     {
-        #         "domainname": "sf2.baz.com",
-        #         "state": "d",
-        #         "gpus": "",
-        #         "np": 16,
-        #         "core_job_map": {}
-        #     }
-        # ]
 
         return worker_nodes
 
@@ -114,7 +65,7 @@ class DemoBatchSystem(GenericBatchSystem):
         job_ids = self.jobs
         # These are used in the upper and lower parts of qtop in the statistics part
         job_states = [random.choice("Q R C E W".split()) for _ in job_ids]
-        usernames = [random.choice("alice023 john lhc154 fotis thomas".split()) for _ in job_ids]
+        usernames = [random.choice("alice023 cmsmu lhc154 fotis Atlassm".split()) for _ in job_ids]
         queue_names = [random.choice("Urgent Foobar Urgent Foobar Priori".split()) for _ in job_ids]
         return job_ids, usernames, job_states, queue_names
 
@@ -131,11 +82,5 @@ class DemoBatchSystem(GenericBatchSystem):
             qstatq['state'] = random.choice("Q R C E W".split())
             qstatq['lm'] = random.randint(0, 100)
             qstatq_list.append(qstatq)
-        # qstatq_list = [
-        #     #                     V- Why these should be strings (colorize)?
-        #     {'run': 2, 'queued': '3', 'queue_name': 'Urgent', 'state': 'Q', 'lm': 0},
-        #     {'run': 2, 'queued': '3', 'queue_name': 'Priori', 'state': 'Q', 'lm': 0},
-        #     {'run': 1, 'queued': '2', 'queue_name': 'Foobar', 'state': 'W', 'lm': 0}
-        # ]
 
         return total_running_jobs, total_queued_jobs, qstatq_list
