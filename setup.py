8970fc1fef2265dc05b0c6a38d96e5471688e3fc
Sotiris Fragkiskos <sfranky@gmail.com>
2016-11-21 18:55:20 +0200

    First setup.py try with new dir structure

--- setup.py
+++ setup.py
@@ -18,10 +18,26 @@ setup(
     author='Sotiris Fragkiskos',
     author_email='sfranky@gmail.com',
     url="https://github.com/qtop/qtop",
-    ## packages=['qtop'],
-    ## install_requires=['bar', 'foo'], #external packages as dependencies
+    packages=['qtop'],
     scripts=[
-        'qtop',
-        ##'scripts/helloworld',
-    ]
+        'inputs/joboutputs.sh',
+        'inputs/jobstatuses.sh',
+        'inputs/jobsubmits.sh',
+        'inputs/qtop-input.jdl',
+        'inputs/qtop-input.sh',
+        'contrib/func_tests.sh',
+        'contrib/oar1_dvv_out.ref',
+        'contrib/oarnodes_s_Y.txt',
+        'contrib/oarnodes_Y.txt',
+        'contrib/oarstat.txt',
+        'contrib/pbs_dvv_out.ref',
+        'contrib/pbsnodes_a.txt',
+        'contrib/qstat.F.xml.stdout',
+        'contrib/qstat.txt',
+        'contrib/qstat_q.txt',
+        'contrib/qtop_demo.gif',
+        'contrib/sger_dvv_out.ref',
+        'web/index.html',
+        'qtop/qtopconf.yaml'
+     ]
 )
