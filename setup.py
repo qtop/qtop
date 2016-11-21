71b39542c95bf3822fdaeea62f0410feee8e358a
Sotiris Fragkiskos <sfranky@gmail.com>
2016-11-21 19:36:40 +0200

    setup py first working attempt

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.rst", 'r') as f:
 
 setup(
     name='qtop',
-    version='0.9.0rc1',
+    version='0.9.0rc3',
     description="""qtop: the fast text mode way to monitor your cluster's utilization and status;
       the time has come to take back control of your cluster's scheduling business""",
     license="MIT",
@@ -19,25 +19,4 @@ setup(
     author_email='sfranky@gmail.com',
     url="https://github.com/qtop/qtop",
     packages=['qtop'],
-    scripts=[
-        'inputs/joboutputs.sh',
-        'inputs/jobstatuses.sh',
-        'inputs/jobsubmits.sh',
-        'inputs/qtop-input.jdl',
-        'inputs/qtop-input.sh',
-        'contrib/func_tests.sh',
-        'contrib/oar1_dvv_out.ref',
-        'contrib/oarnodes_s_Y.txt',
-        'contrib/oarnodes_Y.txt',
-        'contrib/oarstat.txt',
-        'contrib/pbs_dvv_out.ref',
-        'contrib/pbsnodes_a.txt',
-        'contrib/qstat.F.xml.stdout',
-        'contrib/qstat.txt',
-        'contrib/qstat_q.txt',
-        'contrib/qtop_demo.gif',
-        'contrib/sger_dvv_out.ref',
-        'web/index.html',
-        'qtop/qtopconf.yaml'
-     ]
 )
