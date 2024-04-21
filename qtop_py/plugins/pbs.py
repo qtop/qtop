d3418f64d4f795b0e7da75cee01ddb5b5eb94084
Fotis Georgatos <kefalonia@gmail.com>
2024-04-21 23:19:03 +0000

    ruff format qtop_py/plugins/pbs.py

--- qtop_py/plugins/pbs.py
+++ qtop_py/plugins/pbs.py
@@ -15,24 +15,28 @@ import itertools
 class PBSStatExtractor(StatExtractor):
     def __init__(self, config, options):
         StatExtractor.__init__(self, config, options)
-        self.user_q_search = r'^(?P<host_name>(?P<job_id>[0-9\[\]-]+)\.(?P<domain>[\w*-]+))\s+' \
-                             r'(?P<name>[\w%.=+/{}*-]+)\s+' \
-                             r'(?P<user>[A-Za-z0-9.*]+)\s+' \
-                             r'(?P<time>\d+:\d*:?\d*\*?|0)\s+' \
-                             r'(?P<state>[BCEFHMQRSTUWX])\s+' \
-                             r'(?P<queue_name>\w+)'
+        self.user_q_search = (
+            r"^(?P<host_name>(?P<job_id>[0-9\[\]-]+)\.(?P<domain>[\w*-]+))\s+"
+            r"(?P<name>[\w%.=+/{}*-]+)\s+"
+            r"(?P<user>[A-Za-z0-9.*]+)\s+"
+            r"(?P<time>\d+:\d*:?\d*\*?|0)\s+"
+            r"(?P<state>[BCEFHMQRSTUWX])\s+"
+            r"(?P<queue_name>\w+)"
+        )
 
-        self.user_q_search_prior = r'\s{0,2}' \
-                                   r'(?P<job_id>\d+)\s+' \
-                                   r'(?:[0-9]\.[0-9]+)\s+' \
-                                   r'(?:[\w.-]+)\s+' \
-                                   r'(?P<user>[\w.-]+)\s+' \
-                                   r'(?P<state>[a-z])\s+' \
-                                   r'(?:\d{2}/\d{2}/\d{2}|0)\s+' \
-                                   r'(?:\d+:\d+:\d*|0)\s+' \
-                                   r'(?P<queue_name>\w+@[\w.-]+)\s+' \
-                                   r'(?:\d+)\s+' \
-                                   r'(?:\w*)'
+        self.user_q_search_prior = (
+            r"\s{0,2}"
+            r"(?P<job_id>\d+)\s+"
+            r"(?:[0-9]\.[0-9]+)\s+"
+            r"(?:[\w.-]+)\s+"
+            r"(?P<user>[\w.-]+)\s+"
+            r"(?P<state>[a-z])\s+"
+            r"(?:\d{2}/\d{2}/\d{2}|0)\s+"
+            r"(?:\d+:\d+:\d*|0)\s+"
+            r"(?P<queue_name>\w+@[\w.-]+)\s+"
+            r"(?:\d+)\s+"
+            r"(?:\w*)"
+        )
 
     def extract_qstat(self, orig_file):
         """
@@ -51,28 +55,28 @@ class PBSStatExtractor(StatExtractor):
         try:
             fileutils.check_empty_file(orig_file)
         except fileutils.FileEmptyError:
-            logging.error('File %s seems to be empty.' % orig_file)
+            logging.error("File %s seems to be empty." % orig_file)
             all_qstat_values = []
         else:
             try:
                 with open(orig_file) as f:
                     _ = json.load(f)
             except json.JSONDecodeError:
-                logging.info('Extracting qstat output using regex')
+                logging.info("Extracting qstat output using regex")
                 all_qstat_values = self._extract_qstat_regex(orig_file)
             else:
-                logging.info('Extracting qstat output using json')
+                logging.info("Extracting qstat output using json")
                 all_qstat_values = self._extract_qstat_json(orig_file)
 
         return all_qstat_values
-    
+
     def _extract_qstat_regex(self, qstat_file):
         all_qstat_values = list()
-        with open(qstat_file, 'r') as fin:
+        with open(qstat_file, "r") as fin:
             _ = fin.readline()  # header
-            fin.readline() # horizontal row
-            line = fin.readline() # first line
-            re_match_positions = ('job_id', 'user', 'state', 'queue_name')  # was: (1, 5, 7, 8), (1, 4, 5, 8)
+            fin.readline()  # horizontal row
+            line = fin.readline()  # first line
+            re_match_positions = ("job_id", "user", "state", "queue_name")  # was: (1, 5, 7, 8), (1, 4, 5, 8)
             try:  # first qstat line determines which format qstat follows.
                 re_search = self.user_q_search
                 qstat_values = self._process_qstat_line(re_search, line, re_match_positions)
@@ -84,24 +88,24 @@ class PBSStatExtractor(StatExtractor):
                 # m.group(2), m.group(3), m.group(6), m.group(7), m.group(9), m.group(10), m.group(11)
             finally:
                 all_qstat_values.append(qstat_values)
-            
+
             # hence the rest of the lines should follow either try's or except's same format
             for line in fin:
                 qstat_values = self._process_qstat_line(re_search, line, re_match_positions)
                 all_qstat_values.append(qstat_values)
 
         return all_qstat_values
-    
+
     def _extract_qstat_json(self, qstat_file):
         all_qstat_values = list()
-        with open(qstat_file, 'r') as fin:
+        with open(qstat_file, "r") as fin:
             data = json.load(fin)
             jobs = data["Jobs"]
             for job_id, job in jobs.items():
                 qstat_values = dict()
-                user = job["Job_Owner"].split('@')[0]
-                user = self.anonymize(user, 'users')
-                qstat_values["JobId"] = job_id.split('.')[0]
+                user = job["Job_Owner"].split("@")[0]
+                user = self.anonymize(user, "users")
+                qstat_values["JobId"] = job_id.split(".")[0]
                 qstat_values["Queue"] = job["queue"]
                 qstat_values["JobName"] = job["Job_Name"]
                 qstat_values["UnixAccount"] = user
@@ -126,40 +130,42 @@ class PBSStatExtractor(StatExtractor):
         try:
             fileutils.check_empty_file(orig_file)
         except fileutils.FileEmptyError:
-            logging.error('File %s seems to be empty.' % orig_file)
+            logging.error("File %s seems to be empty." % orig_file)
             all_qstatq_values = []
         else:
             try:
                 with open(orig_file) as f:
                     _ = json.load(f)
             except json.JSONDecodeError:
-                logging.info('Extracting qstat_q output using regex')
+                logging.info("Extracting qstat_q output using regex")
                 all_qstatq_values = self._extract_qstatq_regex(orig_file)
             else:
-                logging.info('Extracting qstat_q output using json')
+                logging.info("Extracting qstat_q output using json")
                 all_qstatq_values = self._extract_qstatq_json(orig_file)
 
         return all_qstatq_values
 
     def _extract_qstatq_regex(self, qstatq_file):
         anonymize = self.anonymize_func()
-        queue_search = r'^(?P<queue_name>[\w.-]+)\s+' \
-                        r'(?:--|[0-9]+[mgtkp]b[a-z]*)\s+' \
-                        r'(?:--|\d+:\d+:?\d*:?)\s+' \
-                        r'(?:--|\d+:\d+:?\d+:?)\s+(--)\s+' \
-                        r'(?P<run>\d+)\s+' \
-                        r'(?P<queued>\d+)\s+' \
-                        r'(?P<lm>--|\d+)\s+' \
-                        r'(?P<state>[DE] [RS])'
-        run_qd_search = r'^\s*(?P<tot_run>\d+)\s+(?P<tot_queued>\d+)'  # this picks up the last line contents
+        queue_search = (
+            r"^(?P<queue_name>[\w.-]+)\s+"
+            r"(?:--|[0-9]+[mgtkp]b[a-z]*)\s+"
+            r"(?:--|\d+:\d+:?\d*:?)\s+"
+            r"(?:--|\d+:\d+:?\d+:?)\s+(--)\s+"
+            r"(?P<run>\d+)\s+"
+            r"(?P<queued>\d+)\s+"
+            r"(?P<lm>--|\d+)\s+"
+            r"(?P<state>[DE] [RS])"
+        )
+        run_qd_search = r"^\s*(?P<tot_run>\d+)\s+(?P<tot_queued>\d+)"  # this picks up the last line contents
 
         all_qstatq_values = list()
-        with open(qstatq_file, 'r') as fin:
+        with open(qstatq_file, "r") as fin:
             fin.readline()
             fin.readline()
             # server_name = fin.next().split(': ')[1].strip()
             fin.readline()
-            fin.readline() #.strip()  # the headers line should later define the keys in temp_dict, should they be different
+            fin.readline()  # .strip()  # the headers line should later define the keys in temp_dict, should they be different
             fin.readline()
             for line in fin:
                 line = line.strip()
@@ -167,29 +173,25 @@ class PBSStatExtractor(StatExtractor):
                 n = re.search(run_qd_search, line)
                 temp_dict = {}
                 try:
-                    queue_name = m.group('queue_name') if not self.options.ANONYMIZE else anonymize(m.group('queue_name'), 'qs')
-                    run, queued, lm, state = m.group('run'), m.group('queued'), m.group('lm'), m.group('state')
+                    queue_name = m.group("queue_name") if not self.options.ANONYMIZE else anonymize(m.group("queue_name"), "qs")
+                    run, queued, lm, state = m.group("run"), m.group("queued"), m.group("lm"), m.group("state")
                 except AttributeError:
                     try:
-                        total_running_jobs, total_queued_jobs = n.group('tot_run'), n.group('tot_queued')
+                        total_running_jobs, total_queued_jobs = n.group("tot_run"), n.group("tot_queued")
                     except AttributeError:
                         continue
                 else:
-                    for key, value in [('queue_name', queue_name),
-                                        ('run', run),
-                                        ('queued', queued),
-                                        ('lm', lm),
-                                        ('state', state)]:
+                    for key, value in [("queue_name", queue_name), ("run", run), ("queued", queued), ("lm", lm), ("state", state)]:
                         temp_dict[key] = value
                     all_qstatq_values.append(temp_dict)
-            all_qstatq_values.append({'Total_running': total_running_jobs, 'Total_queued': total_queued_jobs})
+            all_qstatq_values.append({"Total_running": total_running_jobs, "Total_queued": total_queued_jobs})
 
         return all_qstatq_values
-    
+
     def _extract_qstatq_json(self, qstatq_file):
         anonymize = self.anonymize_func()
         all_qstatq_values = list()
-        with open(qstatq_file, 'r') as fin:
+        with open(qstatq_file, "r") as fin:
             data = json.load(fin)
             queues = data["Queue"]
             for queue_name, queue in queues.items():
@@ -198,25 +200,24 @@ class PBSStatExtractor(StatExtractor):
                 qstatq_values["queue_name"] = queue_name
                 qstatq_values["run"] = queue["state_count"].split(" ")[4].split(":")[1]
                 qstatq_values["queued"] = queue["state_count"].split(" ")[1].split(":")[1]
-                qstatq_values["lm"] = "--" # TODO: find value in json
+                qstatq_values["lm"] = "--"  # TODO: find value in json
                 qstatq_values["state"] = "E" if queue["enabled"] == "True" else "D"
                 all_qstatq_values.append(qstatq_values)
             total_running_jobs = sum([int(item["run"]) for item in all_qstatq_values])
             total_queued_jobs = sum([int(item["queued"]) for item in all_qstatq_values])
-            all_qstatq_values.append({'Total_running': total_running_jobs, 'Total_queued': total_queued_jobs})
+            all_qstatq_values.append({"Total_running": total_running_jobs, "Total_queued": total_queued_jobs})
         return all_qstatq_values
 
 
 class PBSBatchSystem(GenericBatchSystem):
-
     @staticmethod
     def get_mnemonic():
         return "pbs"
 
     def __init__(self, scheduler_output_filenames, config, options):
-        self.pbsnodes_file = scheduler_output_filenames.get('pbsnodes_file')
-        self.qstat_file = scheduler_output_filenames.get('qstat_file')
-        self.qstatq_file = scheduler_output_filenames.get('qstatq_file')
+        self.pbsnodes_file = scheduler_output_filenames.get("pbsnodes_file")
+        self.qstat_file = scheduler_output_filenames.get("qstat_file")
+        self.qstatq_file = scheduler_output_filenames.get("qstatq_file")
 
         self.config = config
         self.options = options
@@ -234,32 +235,32 @@ class PBSBatchSystem(GenericBatchSystem):
         anonymize = self.qstat_maker.anonymize_func()
         for block in raw_blocks:
             pbs_values = dict()
-            pbs_values['domainname'] = block['domainname'] if not self.options.ANONYMIZE else anonymize(block['domainname'], 'wns')
+            pbs_values["domainname"] = block["domainname"] if not self.options.ANONYMIZE else anonymize(block["domainname"], "wns")
 
-            nextchar = block['state'][0]
-            state = (nextchar == 'f') and "-" or nextchar
+            nextchar = block["state"][0]
+            state = (nextchar == "f") and "-" or nextchar
 
-            pbs_values['state'] = state
+            pbs_values["state"] = state
 
             # find attribute for number of cores, default is 0
-            if block.get('np'):
-                pbs_values['np'] = block['np']
-            elif block.get('pcpus'):
-                pbs_values['np'] = block['pcpus']
+            if block.get("np"):
+                pbs_values["np"] = block["np"]
+            elif block.get("pcpus"):
+                pbs_values["np"] = block["pcpus"]
             else:
-                pbs_values['np'] = block.get('resources_available.ncpus', 0)
+                pbs_values["np"] = block.get("resources_available.ncpus", 0)
 
-            if block.get('gpus', 0) > 0:  # this should be rare.
-                pbs_values['gpus'] = block['gpus']
+            if block.get("gpus", 0) > 0:  # this should be rare.
+                pbs_values["gpus"] = block["gpus"]
 
             try:  # this should turn up more often, hence the try/except.
-                _ = block['jobs']
+                _ = block["jobs"]
             except KeyError:
-                pbs_values['core_job_map'] = dict()  # change of behaviour: all entries should contain the key even if no value
+                pbs_values["core_job_map"] = dict()  # change of behaviour: all entries should contain the key even if no value
             else:
                 # jobs = re.split(r'(?<=[A-Za-z0-9]),\s?', block['jobs'])
-                jobs = re.findall(r'[0-9][0-9a-zA-Z\[\],.-]*\/[^,]+', block['jobs'])
-                pbs_values['core_job_map'] = dict((core, job) for job, core in self._get_jobs_cores(jobs))
+                jobs = re.findall(r"[0-9][0-9a-zA-Z\[\],.-]*\/[^,]+", block["jobs"])
+                pbs_values["core_job_map"] = dict((core, job) for job, core in self._get_jobs_cores(jobs))
             finally:
                 all_pbs_values.append(pbs_values)
 
@@ -276,20 +277,16 @@ class PBSBatchSystem(GenericBatchSystem):
 
         qstats = self.qstat_maker.extract_qstat(self.qstat_file)
         for qstat in qstats:
-            job_ids.append(re.sub(r'\[\]$', '', str(qstat['JobId'])))
-            usernames.append(qstat['UnixAccount'])
-            job_states.append(qstat['S'])
-            queue_names.append(qstat['Queue'])
+            job_ids.append(re.sub(r"\[\]$", "", str(qstat["JobId"])))
+            usernames.append(qstat["UnixAccount"])
+            job_states.append(qstat["S"])
+            queue_names.append(qstat["Queue"])
 
-        logging.debug('job_ids, usernames, job_states, queue_names lengths: '
-                      '%(job_ids)s, %(usernames)s, %(job_states)s, %(queue_names)s'
-                      % {
-                          "job_ids": len(job_ids),
-                          "usernames": len(usernames),
-                          "job_states": len(job_states),
-                          "queue_names": len(queue_names)
-                      }
-                      )
+        logging.debug(
+            "job_ids, usernames, job_states, queue_names lengths: "
+            "%(job_ids)s, %(usernames)s, %(job_states)s, %(queue_names)s"
+            % {"job_ids": len(job_ids), "usernames": len(usernames), "job_states": len(job_states), "queue_names": len(queue_names)}
+        )
         return job_ids, usernames, job_states, queue_names
 
     def get_queues_info(self):
@@ -304,7 +301,7 @@ class PBSBatchSystem(GenericBatchSystem):
         for qstatq in qstatqs_total[:-1]:
             qstatq_list.append(qstatq)
         for _total in qstatqs_total[-1:]:  # this is at most one item
-            total_running_jobs, total_queued_jobs = _total['Total_running'], _total['Total_queued']
+            total_running_jobs, total_queued_jobs = _total["Total_running"], _total["Total_queued"]
             break
         else:
             total_running_jobs, total_queued_jobs = 0, 0
@@ -321,26 +318,26 @@ class PBSBatchSystem(GenericBatchSystem):
         and spits tuples of the format (0, 10102182)    (job,core)
         """
         for core_job in jobs:
-            part1, part2 = core_job.strip().split('/')
-            if re.search(r'^\d+[,-]?[\d,-]*$', part1): # Torque job id
+            part1, part2 = core_job.strip().split("/")
+            if re.search(r"^\d+[,-]?[\d,-]*$", part1):  # Torque job id
                 core, job = part1, part2
-            elif re.match(r'[\w.-]+', part1): # PBS Pro job id
+            elif re.match(r"[\w.-]+", part1):  # PBS Pro job id
                 job, core = part1, part2
 
-            if (',' in core) or ('-' in core): # job id with subjobs
-                for (subcore, subjob) in PBSBatchSystem.get_corejob_from_range(core, job):
-                    subjob = subjob.strip().split('/')[0].split('.')[0]
-                    yield subjob, subcore # TODO: int or no int?
-            else: # job id without subjobs
-                job = job.strip().split('/')[0].split('.')[0]
-                job = re.sub(r'\[\d*\]$', '', job)
+            if ("," in core) or ("-" in core):  # job id with subjobs
+                for subcore, subjob in PBSBatchSystem.get_corejob_from_range(core, job):
+                    subjob = subjob.strip().split("/")[0].split(".")[0]
+                    yield subjob, subcore  # TODO: int or no int?
+            else:  # job id without subjobs
+                job = job.strip().split("/")[0].split(".")[0]
+                job = re.sub(r"\[\d*\]$", "", job)
                 yield job, core
 
     def _read_all_blocks(self, orig_file):
         """
         reads pbsnodes txt file block by block
         """
-        with open(orig_file, mode='r') as fin:
+        with open(orig_file, mode="r") as fin:
             result = []
             reading = True
             while reading:
@@ -357,15 +354,15 @@ class PBSBatchSystem(GenericBatchSystem):
         if not domain_name:
             return None
 
-        block = {'domainname': domain_name}
+        block = {"domainname": domain_name}
         reading = True
         while reading:
             line = fin.readline()
-            if line == '\n':
+            if line == "\n":
                 reading = False
             else:
                 try:
-                    key, value = line.split(' = ')
+                    key, value = line.split(" = ")
                 except ValueError:  # e.g. if line is 'jobs =' with no jobs
                     pass
                 else:
@@ -375,10 +372,10 @@ class PBSBatchSystem(GenericBatchSystem):
     @staticmethod
     def get_corejob_from_range(core_selections, job):
         _cores = list()
-        subselections = core_selections.split(',')
+        subselections = core_selections.split(",")
         for subselection in subselections:
-            if '-' in subselection:
-                range_ = list(map(int, subselection.split('-')))
+            if "-" in subselection:
+                range_ = list(map(int, subselection.split("-")))
                 range_[-1] += 1
                 _cores.extend([map(str, range(*range_))])
             else:
