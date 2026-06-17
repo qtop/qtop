63c2278572d5854d658ba0e46c4a56b819323d71
Utkarsh Sinha <159023661+Utkarsh-Sinha0@users.noreply.github.com>
2026-06-17 21:18:15 +0530

    20260617 (human): tighten CLI entrypoint scheduler errors
    Signed-off-by: Utkarsh Sinha <159023661+Utkarsh-Sinha0@users.noreply.github.com>

--- qtop_py/cli.py
+++ qtop_py/cli.py
@@ -10,24 +10,7 @@
 
 import re
 import sys
-from qtop_py.qtop import InvalidScheduler, NoSchedulerFound, SchedulerNotSpecified, main
-
-
-def cli_main():
-    try:
-        return main() or 0
-    except SchedulerNotSpecified:
-        # fmt: off
-        sys.stderr.write(
-            "No scheduler could be auto-detected. "
-            "Select one with -b/--batchSystem, QTOP_SCHEDULER, or qtopconf.yaml.\n"
-        )
-        # fmt: on
-        return 1
-    except (InvalidScheduler, NoSchedulerFound) as exc:
-        if str(exc):
-            sys.stderr.write("%s\n" % exc)
-        return 1
+from qtop_py.qtop import cli_main
 
 
 if __name__ == "__main__":
